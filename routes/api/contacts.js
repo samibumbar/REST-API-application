ac0f00e786e99dbfd045e00b2bf219668c1c5f5d
OMBRENORMAL <134549221+samibumbar@users.noreply.github.com>
2025-01-17 19:58:11 +0100

    fully functional REST API

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,55 @@
-const express = require('express')
+import express from "express";
+import {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} from "../../models/contacts.js";
+import { validateContact } from "../../utils/validation.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res) => {
+  const contacts = await listContacts();
+  res.status(200).json(contacts);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res) => {
+  const contact = await getContactById(req.params.contactId);
+  if (!contact) {
+    return res.status(404).json({ message: "Not found" });
+  }
+  res.status(200).json(contact);
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res) => {
+  const { error } = validateContact(req.body);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  const newContact = await addContact(req.body);
+  res.status(201).json(newContact);
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res) => {
+  const deleted = await removeContact(req.params.contactId);
+  if (!deleted) {
+    return res.status(404).json({ message: "Not found" });
+  }
+  res.status(200).json({ message: "Contact deleted" });
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.put("/:contactId", async (req, res) => {
+  const { error } = validateContact(req.body, true);
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+  const updatedContact = await updateContact(req.params.contactId, req.body);
+  if (!updatedContact) {
+    return res.status(404).json({ message: "Not found" });
+  }
+  res.status(200).json(updatedContact);
+});
 
-module.exports = router
+export default router;
