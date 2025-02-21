b53a4a6dd25d3585634a5d14a785f9dc93bb74f2
OMBRENORMAL <134549221+samibumbar@users.noreply.github.com>
2025-02-21 17:48:51 +0100

    Added e-mail verification and dockerfile

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,47 +1,122 @@
 import { User } from "../models/userModel.js";
-
+import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import gravatar from "gravatar";
+import sendVerificationEmail from "../services/emailService.js";
 
 export const signup = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    return res.status(409).json({ message: "Email in use" });
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: "Email in use" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email, { s: "250", d: "retro" }, true);
+    const verificationToken = uuidv4();
+
+    const user = await User.create({
+      email,
+      password: hashedPassword,
+      avatarURL,
+      verify: false,
+      verificationToken,
+    });
+
+    await sendVerificationEmail(email, verificationToken);
+
+    res.status(201).json({
+      email: user.email,
+      subscription: user.subscription,
+      avatarURL: user.avatarURL,
+      message: "Verification email sent. Please check your inbox.",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
   }
+};
+export const verifyEmail = async (req, res) => {
+  try {
+    const { verificationToken } = req.params;
+    const user = await User.findOne({ verificationToken });
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const avatarURL = gravatar.url(email, { s: "250", d: "retro" }, true);
-  const user = await User.create({
-    email,
-    password: hashedPassword,
-    avatarURL,
-  });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
-  res.status(201).json({
-    email: user.email,
-    subscription: user.subscription,
-    avatarURL: user.avatarURL,
-  });
+    await User.updateOne(
+      { _id: user._id },
+      { $set: { verify: true }, $unset: { verificationToken: "" } }
+    );
+
+    res.json({ message: "Verification successful" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const resendVerificationEmail = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ message: "Missing required field email" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (user.verify) {
+      return res
+        .status(400)
+        .json({ message: "Verification has already been passed" });
+    }
+
+    await sendVerificationEmail(email, user.verificationToken);
+    res.json({ message: "Verification email sent" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const login = async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  try {
+    console.log("Login attempt:", req.body);
 
-  if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.status(401).json({ message: "Email or password is wrong" });
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      console.log("User not found");
+      return res.status(401).json({ message: "Email or password is wrong" });
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: "Email or password is wrong" });
+    }
+
+    if (!user.verify) {
+      console.log("User email not verified");
+      return res.status(403).json({ message: "Email not verified" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    user.token = token;
+    await user.save();
+
+    res.json({ token, email: user.email, subscription: user.subscription });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
-
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
-  });
-  user.token = token;
-  await user.save();
-
-  res.json({ token, email: user.email, subscription: user.subscription });
 };
 
 export const logout = async (req, res) => {
